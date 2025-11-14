37c7bf86a50e184ba8f65d8387ca20363de3e982
kawanaka223 <n.kwnk4969@gmail.com>
2025-11-14 18:17:31 +0900

    設定をsetting.jsonから読み取るためのexample.pyを作成

--- src/example.py
+++ src/example.py
@@ -1,1 +1,15 @@
-print('This is a example code')
+import json, os
+
+def load_json():
+    src_dir = os.path.dirname(__file__)
+    json_path = os.path.join(src_dir,"setting.json")
+    setting_dict = json.load(open(json_path,"r"))
+    
+    username = setting_dict["username"]
+    print_username = setting_dict["print_username"]
+    
+    if print_username:
+        print(username)
+
+if __name__ == "__main__":
+    load_json()
