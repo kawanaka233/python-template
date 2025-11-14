44dea353ad565baa89e8585ca6cb029343fab558
kawanaka223 <n.kwnk4969@gmail.com>
2025-11-14 19:02:42 +0900

    example.pyのload_json関数をPRに対するコメントに基づいて修正

--- src/example.py
+++ src/example.py
@@ -1,12 +1,16 @@
-import json, os
+import json
+import os
 
 def load_json():
     src_dir = os.path.dirname(__file__)
     json_path = os.path.join(src_dir,"setting.json")
-    setting_dict = json.load(open(json_path,"r"))
+    try:
+        setting_dict = json.load(open(json_path,"r"))
+    except FileNotFoundError as e:
+        print(f"setting.jsonが見つかりません: {e}")
     
     username = setting_dict["username"]
-    print_username = setting_dict["print_username"]
+    print_username = eval(setting_dict["print_username"])
     
     if print_username:
         print(username)
